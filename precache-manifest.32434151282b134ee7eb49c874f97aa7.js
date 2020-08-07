2cdcba8779a11a9cf75c8d7519b9c81b5140378b
tony <tony.aldon.adm@gmail.com>
2020-08-07 12:53:40 +0200

    Updates

---
+++ precache-manifest.32434151282b134ee7eb49c874f97aa7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84d4e3017b83b75773262c6a853e288",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ee2b4218ecbae3a7f46",
+    "url": "/static/css/2.c0d3b3fa.chunk.css"
+  },
+  {
+    "revision": "2814225d09272bda6566",
+    "url": "/static/css/main.52683403.chunk.css"
+  },
+  {
+    "revision": "6ee2b4218ecbae3a7f46",
+    "url": "/static/js/2.1bcb1516.chunk.js"
+  },
+  {
+    "revision": "eab6a173e87a331f09476204845ab06a",
+    "url": "/static/js/2.1bcb1516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2814225d09272bda6566",
+    "url": "/static/js/main.3f976b80.chunk.js"
+  },
+  {
+    "revision": "cc0b7538f00c2a91bc42",
+    "url": "/static/js/runtime-main.3e77c399.js"
+  },
+  {
+    "revision": "6a2ddad1092a0a1c326b6d0e738e682b",
+    "url": "/static/media/materialdesignicons-webfont.6a2ddad1.ttf"
+  },
+  {
+    "revision": "8a67040660444e8d8ee82a406d580d8f",
+    "url": "/static/media/materialdesignicons-webfont.8a670406.eot"
+  },
+  {
+    "revision": "c32505e8c654310ece1da4e1c10eeb57",
+    "url": "/static/media/materialdesignicons-webfont.c32505e8.woff"
+  },
+  {
+    "revision": "dc85ceeb0daba687e36d8dde4ed4d352",
+    "url": "/static/media/materialdesignicons-webfont.dc85ceeb.woff2"
+  }
+]);
